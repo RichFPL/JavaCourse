5f07de0db458e621c4771cf22bfeccc64dd8b895
FocusFPL <vladlat2013@mail.ru>
2025-10-08 16:57:42 +0300

    Added task 3

--- src/homework_java/HomeworkThirdLesson.java
+++ src/homework_java/HomeworkThirdLesson.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HomeworkThirdLesson {
     public static void main(String[] args) {
 
-        outdoorTemperature();
+        getSquares();
     }
 
     //Задача 1:
@@ -43,5 +43,42 @@ public class HomeworkThirdLesson {
     }
     //Задача 3:
     //Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+    public static void getSquares(){
+        int a = 10;
+        int b = 11;
+        int c = 12;
+        int d = 13;
+        int e = 14;
+        int f = 15;
+        int g = 16;
+        int i= 17;
+        int u = 18;
+        int o = 19;
+        int j = 20;
+        Double squarerootA = Math.pow(a,0.5);
+        System.out.println("The Square of a Given Number 10 = " + squarerootA);
+        Double squarerootB = Math.pow(b,0.5);
+        System.out.println("The Square of a Given Number 11 = " + squarerootB);
+        Double squarerootC = Math.pow(c,0.5);
+        System.out.println("The Square of a Given Number 12 = " + squarerootC);
+        Double squarerootD = Math.pow(d,0.5);
+        System.out.println("The Square of a Given Number 13 = " + squarerootD);
+        Double squarerootE = Math.pow(e,0.5);
+        System.out.println("The Square of a Given Number 14 = " + squarerootE);
+        Double squarerootF = Math.pow(f,0.5);
+        System.out.println("The Square of a Given Number 15 = " + squarerootF);
+        Double squarerootG = Math.pow(g,0.5);
+        System.out.println("The Square of a Given Number 16 = " + squarerootG);
+        Double squarerootI = Math.pow(i,0.5);
+        System.out.println("The Square of a Given Number 17 = " + squarerootI);
+        Double squarerootU = Math.pow(u,0.5);
+        System.out.println("The Square of a Given Number 18 = " + squarerootU);
+        Double squarerootO = Math.pow(o,0.5);
+        System.out.println("The Square of a Given Number 19 = " + squarerootO);
+        Double squarerootJ = Math.pow(j,0.5);
+        System.out.println("The Square of a Given Number 20 = " + squarerootJ);
 
+
+
+    }
 }
