356808445b710662cf0fdabc26f2f50c74fcebca
FocusFPL <vladlat2013@mail.ru>
2025-10-14 23:35:18 +0300

    In work 4 homework

--- src/homework_java/HomeworkFourLessonFourthLesson.java
+++ src/homework_java/HomeworkFourLessonFourthLesson.java
@@ -1,24 +1,76 @@
 package homework_java;
 
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HomeworkFourLessonFourthLesson {
     public static void main(String[] args) {
         getRemoval();
 
     }
-//Задача 1:
-//Пройти по массиву, вывести все элементы в прямом и в обратном порядке
+    //Для всех задач исходные условия следующие: пользователь с клавиатуры вводит размер
+    //массива (просто целое число). После того, как размер массива задан, заполнить его
+    //одним из двух способов: используя Math.random(), или каждый элемент массива вводится
+    //пользователем вручную. Попробовать оба варианта. После заполнения массива
+    //данными, решить для него следующие задачи:
+
+
+
     public static void getRemoval(){
-        int[] numbers = new int[]{1, 5, 10, 15, 20};
-        System.out.println("Массив в правильном порядке:");
+        Scanner numberScanner = new Scanner(System.in);
+        System.out.println("Введите длину массива");
+        int importNumbers = numberScanner.nextInt();
+        int[] array = new int[importNumbers];
+        System.out.println("Ваш массив состоит из " + importNumbers +" элементов, и выглядит на данный " +
+                "момент вот так: " + Arrays.toString(array));
 
-        for(int index = 0; index<numbers.length; index++){
-            System.out.println(numbers[index]);
+        for(int index = 0; index< array.length; index++){
+            array[index]=((int)(Math.random()*10));
         }
+
         System.out.println("-".repeat(50));
-        System.out.println("Массив в обратном порядке:");
 
-        for(int reverse = numbers.length-1; reverse>=0; reverse--){
-            System.out.println(numbers[reverse]);
+        //Задача 1:
+        //Пройти по массиву, вывести все элементы в прямом и в обратном порядке
+        System.out.println("Массив в прямом порядке: ");
+        for(int counter = 0; counter< array.length;counter++){
+            System.out.println(array[counter]);
         }
-    }
+
+        System.out.println("Массив в обратном порядке");
+        for(int reverse = array.length - 1; reverse>=0; reverse--){
+            System.out.println(array[reverse]);
+        }
+
+        System.out.println("-".repeat(50));
+
+        //Задача 2:
+        //Найти минимальный-максимальный элементы и вывести в консоль.
+
+        System.out.println("Максимальный элемент в массиве: ");
+        int max = Arrays.stream(array).max().getAsInt();
+        System.out.println(max);
+
+        System.out.println("Минимальный элемент в массиве");
+        int min = Arrays.stream(array).min().getAsInt();
+        System.out.println(min);
+
+        System.out.println("-".repeat(50));
+
+        //Задача 4:
+        //Найти и вывести количество нулевых элементов. Если нулевых элементов нет - вывести
+        //сообщение, что их нет.
+        int zeroCount = 0;
+        for(int zero = 0; zero< array.length; zero++){
+            if(array[zero] == 0){
+                zeroCount++;
+            }else if(array[zero] != 0){
+            }
+        }
+        System.out.println("Количество нулевых элементов в массиве: " + zeroCount);
+        System.out.println("Нулевых элементов в массиве не найдено");
+        }
 }
+
--- src/practice_java/Test.java
+++ src/practice_java/Test.java
@@ -1,6 +1,6 @@
 package practice_java;
 
-import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test {
@@ -8,26 +8,46 @@ public class Test {
        extendArray();
     }
 
-    public static void extendArray(){
-        int[] best = new int[]{-1, 5, 10, 15, -5, 8};
-        int[] theBest = new int[best.length + 10];
-        for(int index = 9, bestIndex = 0; bestIndex<best.length; index++, bestIndex++){
-            theBest[index] = best[bestIndex];
+    public static void extendArray() {
+/*
+        int n = 42;
+        double[] randoms = new double[n];
+        for(int i = 0; i<n; i++){
+            randoms[i] = Math.random();
+            System.out.println(randoms[i]*100);
         }
 
-        for(int index = 0; index < theBest.length; index++){
-            System.out.println("New names[" + index + "]: " + theBest[index] );
+        int[] nums = new int[]{5,10,15,20,25};
+        for (int index = 0; index< nums.length;index++){
+            nums[index] = index + 1;
         }
+
+        String[] names = new String[]{"Vova", "Vlad", "Ksuha"};
+        for(String name : names){
+            System.out.println(name);
+        }
+
+        int[] newNum = new int[]{1,5,8,10,12};
+            System.out.println(Arrays.toString(newNum));
+            Arrays.sort(newNum);
+        System.out.println(Arrays.toString(newNum));
+
+
+        int[] changer = new int[]{12,3,8,16,20};
+        Arrays.sort(changer);
+        for(int newChanger : changer){
+            System.out.println(newChanger);
+        }
+
+ */
+        int[] array = new int[]{5, 7, 10, 12, 15};
+        int[] newArray = new int[]{0,0,0,0,0,0,0,0,0,0};
+
+        System.out.println("Default string array: " + Arrays.toString(array));
+        System.out.println("Default string newArray: " + Arrays.toString(newArray));
+
+        System.arraycopy(array,0, newArray,2,3);
+        System.out.println("newArray after copy: " + Arrays.toString(newArray));
     }
-
-    public static void useMathLibrary(){
-
-        String[] names = new String[]{"Vladik", "Lizka","Dimka","Tomka","Vovka"};
-        for (int index = 0; index < names.length; index++){
-            System.out.println("Your name is " + names[index]);
-        }
-    }
-
-
 
 }
