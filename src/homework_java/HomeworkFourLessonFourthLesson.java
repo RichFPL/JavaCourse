9e6915db563ccd2f4f88aa398764990311dcc7bb
FocusFPL <vladlat2013@mail.ru>
2025-10-14 10:50:32 +0300

    Added new lesson

--- src/homework_java/HomeworkFourLessonFourthLesson.java
+++ src/homework_java/HomeworkFourLessonFourthLesson.java
@@ -9,13 +9,13 @@ public class HomeworkFourLessonFourthLesson {
 //Пройти по массиву, вывести все элементы в прямом и в обратном порядке
     public static void getRemoval(){
         int[] numbers = new int[]{1, 5, 10, 15, 20};
-        System.out.println("Массив в правильном порядке");
+        System.out.println("Массив в правильном порядке:");
 
         for(int index = 0; index<numbers.length; index++){
             System.out.println(numbers[index]);
         }
         System.out.println("-".repeat(50));
-        System.out.println("Массив в обратном порядке");
+        System.out.println("Массив в обратном порядке:");
 
         for(int reverse = numbers.length-1; reverse>=0; reverse--){
             System.out.println(numbers[reverse]);
