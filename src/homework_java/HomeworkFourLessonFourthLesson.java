bc38dae8eda6cfbebebb8f6a5b7b390e4bfd64f4
FocusFPL <vladlat2013@mail.ru>
2025-10-15 10:10:53 +0300

    Fixed 4 homework

--- src/homework_java/HomeworkFourLessonFourthLesson.java
+++ src/homework_java/HomeworkFourLessonFourthLesson.java
@@ -17,17 +17,16 @@ public class HomeworkFourLessonFourthLesson {
     //данными, решить для него следующие задачи:
 
 
-
-    public static void getRemoval(){
+    public static void getRemoval() {
         Scanner numberScanner = new Scanner(System.in);
         System.out.println("Введите длину массива");
         int importNumbers = numberScanner.nextInt();
         int[] array = new int[importNumbers];
-        System.out.println("Ваш массив состоит из " + importNumbers +" элементов, и выглядит на данный " +
+        System.out.println("Ваш массив состоит из " + importNumbers + " элементов, и выглядит на данный " +
                 "момент вот так: " + Arrays.toString(array));
 
-        for(int index = 0; index< array.length; index++){
-            array[index]=((int)(Math.random()*10));
+        for (int index = 0; index < array.length; index++) {
+            array[index] = ((int) (Math.random() * 10));
         }
 
         System.out.println("-".repeat(50));
@@ -35,12 +34,12 @@ public class HomeworkFourLessonFourthLesson {
         //Задача 1:
         //Пройти по массиву, вывести все элементы в прямом и в обратном порядке
         System.out.println("Массив в прямом порядке: ");
-        for(int counter = 0; counter< array.length;counter++){
+        for (int counter = 0; counter < array.length; counter++) {
             System.out.println(array[counter]);
         }
 
         System.out.println("Массив в обратном порядке");
-        for(int reverse = array.length - 1; reverse>=0; reverse--){
+        for (int reverse = array.length - 1; reverse >= 0; reverse--) {
             System.out.println(array[reverse]);
         }
 
@@ -63,14 +62,15 @@ public class HomeworkFourLessonFourthLesson {
         //Найти и вывести количество нулевых элементов. Если нулевых элементов нет - вывести
         //сообщение, что их нет.
         int zeroCount = 0;
-        for(int zero = 0; zero< array.length; zero++){
-            if(array[zero] == 0){
+        for (int zero = 0; zero < array.length; zero++) {
+            if (array[zero] == 0) {
                 zeroCount++;
-            }else if(array[zero] != 0){
+                System.out.println("Количество нулевых элементов в массиве: " + zeroCount);
+            } else {
+                System.out.println("Нулевых элементов в массиве не найдено");
             }
         }
-        System.out.println("Количество нулевых элементов в массиве: " + zeroCount);
-        System.out.println("Нулевых элементов в массиве не найдено");
-        }
+        //Доработать вывод сообщений
+    }
 }
 
