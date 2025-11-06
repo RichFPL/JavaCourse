4f4b6c50b93784ebf70159a5befe37a19337dc8b
FocusFPL <vladlat2013@mail.ru>
2025-11-06 12:12:11 +0300

    Added lesson 10

---
+++ src/lesson_10_object_this/interf/ComparableInterface.java
@@ -0,0 +1,8 @@
+package lesson_10_object_this.interf;
+
+// INTERFACE - IS FULLY ABSTRACT CLASS
+// КОТОРЫЙ НИЧЕГО НЕ ХРАНИТ, У НЕГО НЕТ ДАННЫХ
+// Functional Interface - функциональный интерфейс
+public interface ComparableInterface {
+    boolean compare(); // ГДЕ-ТО КАК-ТО СТАНДАРТНО РЕАЛИЗОВАН
+}
---
+++ src/lesson_9_abstracts/controlpanel/ControlInternet.java
@@ -0,0 +1,9 @@
+package lesson_9_abstracts.controlpanel;
+
+public interface ControlInternet {
+    public void turnOnInternet();
+    public void turnOffInternet();
+    public void speedTest();
+    public void smartTV();
+    public String getInternetInformation();
+}
